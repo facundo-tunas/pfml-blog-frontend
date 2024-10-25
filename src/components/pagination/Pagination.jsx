2eb490deb15fc7b4272c54fe900054d90522daff
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-10-25 18:24:58 -0300

    dynamically show only five page buttons

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,10 +1,36 @@
 import PropTypes from "prop-types";
 import styles from "./Pagination.module.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Pagination = ({ totalPages, handlePageChange, currentPage }) => {
+  const [indexes, setIndexes] = useState([]);
+  useEffect(() => {
+    const newIndexes = [];
+
+    if (totalPages <= 5) {
+      for (let i = 0; i < totalPages; i++) {
+        newIndexes.push(i);
+      }
+    } else {
+      const start = Math.max(0, currentPage - 3);
+      let end = Math.min(totalPages, currentPage + 2);
+
+      if (currentPage == 1) {
+        end = end + 2;
+      }
+
+      for (let i = start; i < end; i++) {
+        newIndexes.push(i);
+      }
+    }
+
+    setIndexes(newIndexes);
+  }, [currentPage, totalPages]);
+
   return (
     <div className={styles.container}>
-      {Array.from({ length: totalPages }).map((_, index) => (
+      {indexes.map((index) => (
         <button
           key={index}
           className={index + 1 === currentPage ? styles.activePage : ""}
