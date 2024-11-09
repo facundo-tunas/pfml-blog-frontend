f0a84e85691236069acfd5b1502ede07fe502444
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-11-08 23:54:00 -0300

    small styling fixes

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,17 +1,19 @@
 import PropTypes from "prop-types";
 import styles from "./User.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const User = ({ user }) => {
+  const [userPicture, setUserPicture] = useState("/test-image-user.png");
+
   useEffect(() => {
-    if (!user.picture) {
-      user.picture = "/test-image-user.png";
+    if (user.picture) {
+      setUserPicture(user.picture);
     }
   }, [user]);
 
   return (
     <div className={styles.userContainer}>
-      <img className={styles.picture} src={user.picture} alt="" />
+      <img className={styles.picture} src={userPicture} alt="" />
       <span className={styles.username}>{user.username}</span>
     </div>
   );
