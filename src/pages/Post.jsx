e960acdf5b3b961f82607f6939db97872a0b5d27
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-11-10 02:30:26 -0300

    add code syntax highlighting

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,10 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+import rehypeRaw from "rehype-raw";
 import Loading from "../components/Loading";
 import LoadingContext from "../contexts/LoadingContext";
 import PopupContext from "/src/contexts/PopupContext";
-
 import styles from "./Post.module.css";
 import MainCard from "../components/blog-cards/MainCard";
 import CommentsSection from "../components/comments/CommentsSection";
@@ -13,9 +15,7 @@ import ModeButton from "../components/modeButton/ModeButton";
 const Post = () => {
   let { id } = useParams();
   const [post, setPost] = useState(null);
-
   const { showPopup } = useContext(PopupContext);
-
   const { fadeOut, loading, setLoading, setFadeOut } =
     useContext(LoadingContext);
 
@@ -31,14 +31,11 @@ const Post = () => {
         const response = await fetch(
           `${import.meta.env.VITE_API_URL}/posts/${id}`
         );
-
         if (!response.ok) {
           throw new Error("Failed to fetch post.");
         }
-
         const data = await response.json();
         setPost(data);
-        console.log(post);
       } catch (error) {
         showPopup(error.message);
       } finally {
@@ -57,7 +54,6 @@ const Post = () => {
         <Loading
           style={{
             opacity: fadeOut ? 0 : 1,
-
             background: "var(--loading-white)",
           }}
         />
@@ -82,10 +78,31 @@ const Post = () => {
                   </p>
                 </div>
               </div>
-
-              <ReactMarkdown>{post.content}</ReactMarkdown>
+              <ReactMarkdown
+                rehypePlugins={[rehypeRaw]}
+                components={{
+                  code({ inline, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className || "");
+                    return !inline && match ? (
+                      <SyntaxHighlighter
+                        style={vscDarkPlus}
+                        language={match[1]}
+                        PreTag="div"
+                        {...props}
+                      >
+                        {String(children).replace(/\n$/, "")}
+                      </SyntaxHighlighter>
+                    ) : (
+                      <code className={className} {...props}>
+                        {children}
+                      </code>
+                    );
+                  },
+                }}
+              >
+                {post.content}
+              </ReactMarkdown>
             </div>
-
             <CommentsSection postId={post.id} />
           </div>
         </>
@@ -96,4 +113,5 @@ const Post = () => {
     </main>
   );
 };
+
 export default Post;
